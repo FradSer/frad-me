1447ef477356440da248ba11c3af9b165edd21ee
Frad LEE <fradser@gmail.com>
2022-02-05 14:09:20 +0800

    refactor(loading): rename props

--- pages/loading.tsx
+++ pages/loading.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 // https://www.bhagyamudgal.me/blog/how-to-make-custom-loading-screen-in-nextjs-project
 
-type Props = {
+type ILoadingProps = {
   loading: boolean;
 };
 
-export default function Loading<T extends Props>(props: T) {
+export default function Loading<T extends ILoadingProps>(props: T) {
   const classNames = props.loading
     ? 'w-full h-full flex items-center justify-center'
     : 'hidden';
