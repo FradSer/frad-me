ab82f25ef25603544bc20b45c26266e07fbb4aca
Frad LEE <fradser@gmail.com>
2022-02-06 02:31:59 +0800

    refactor(mouse): replace use window size hook

--- hooks/useMousePosition.ts
+++ hooks/useMousePosition.ts
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 // https://dev.to/holdmypotion/react-custom-cursor-no-extra-dependencies-25ki
 
 export default function useMousePosition() {
-  const [mousePosition, setMousePosition] = useState({ x: null, y: null });
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     const mouseMoveHandler = (event: any) => {
