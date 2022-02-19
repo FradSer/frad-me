d8d5e248f8958e4266a32f1a64ec5e7c6f488403
Frad LEE <fradser@gmail.com>
2022-02-20 02:59:06 +0800

    feat(landing): add max display works filter

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,8 @@ import WorkCard from '../components/Landing/WorkCard';
 
 import workLinks from '../content/workLinks';
 
+const MAX_DISPLAY_WORKS = 5;
+
 export default function Home() {
   return (
     <>
@@ -15,7 +17,7 @@ export default function Home() {
       <main className="bg-white dark:bg-black flex flex-col w-full items-center justify-center min-h-screen py-2 text-center">
         <Hero />
         <section className="w-screen grid gap-4 grid-cols-2">
-          {workLinks.map((link) => (
+          {workLinks.slice(0, MAX_DISPLAY_WORKS).map((link) => (
             <WorkCard
               key={link.title}
               title={link.title}
