e0b26ae16ccd8b4ef481288d284416b0bccac353
Frad LEE <fradser@gmail.com>
2022-03-03 10:47:34 +0800

    feat(work): add image animation

--- components/WorkPage/WorkImage.tsx
+++ components/WorkPage/WorkImage.tsx
@@ -1,21 +1,46 @@
 import classNames from 'classnames';
+import { motion } from 'framer-motion';
 
 type IWorkImageProps = {
-  underH2?: boolean;
+  position: ImagePosition;
 };
 
-function WorkImage({ underH2 }: IWorkImageProps) {
-  const workImageClass = classNames(
-    'col-span-16 mt-0 h-32 bg-black md:col-span-5',
-    {
-      'md:mt-[-3.25rem]': underH2,
-    }
+enum ImagePosition {
+  inline = 'inline',
+  underH2 = 'underH2',
+  fullScreen = 'fullScreen',
+}
+
+function WorkImage({ position }: IWorkImageProps) {
+  const workImageClass = classNames({
+    'col-span-16 mt-0 h-32 bg-black md:col-span-5':
+      position === ImagePosition.inline,
+    'col-span-16 w-full h-screen bg-black':
+      position === ImagePosition.fullScreen,
+    'col-span-16 mt-0 h-32 bg-black md:col-span-5 md:mt-[-3.25rem]':
+      position === ImagePosition.underH2,
+  });
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: '38%' }}
+      whileInView={{
+        opacity: 1,
+        y: 0,
+        transition: {
+          duration: 0.6,
+          delay: 0.2,
+          ease: 'easeIn',
+        },
+      }}
+      viewport={{ once: true }}
+      className={workImageClass}
+    ></motion.div>
   );
-  return <div className={workImageClass}></div>;
 }
 
 WorkImage.defaultProps = {
-  underH2: true,
+  position: ImagePosition.inline,
 };
 
 export default WorkImage;
