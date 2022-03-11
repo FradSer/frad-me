1b80d73a80cf8f237b8a517d0891f0dae4e04ed5
Frad LEE <fradser@gmail.com>
2022-03-12 01:48:39 +0800

    refactor(landing): add h1 class name

--- components/Landing/Hero/index.tsx
+++ components/Landing/Hero/index.tsx
@@ -1,12 +1,23 @@
+import classNames from 'classnames';
+
 import ScrollLink from '../../common/ScrollLink';
 import DotCircle from './DotCircle';
 import Rectangle from './Rectangle';
 import Triangle from './Triangle';
 
 export default function Hero() {
+  const heroH1 = classNames(
+    'text-left text-2xl font-bold hover:cursor-default sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl'
+  );
+
   return (
     <section className="m-auto flex h-auto items-center justify-center">
-      <h1 className="flex flex-col items-start justify-center text-left text-2xl font-bold hover:cursor-default sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl">
+      <h1
+        className={classNames(
+          'flex flex-col items-start justify-center',
+          heroH1
+        )}
+      >
         <div className="relative">
           <div className="absolute bottom-10 -left-12 z-30 sm:-left-16 sm:bottom-12 lg:-left-24 lg:bottom-16 2xl:-left-28 2xl:bottom-20">
             <Triangle />
