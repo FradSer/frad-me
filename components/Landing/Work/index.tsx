2af189f6a73702fab71f946992194820326df87c
Frad LEE <fradser@gmail.com>
2022-03-12 01:54:59 +0800

    feat(landing): update work ux

--- components/Landing/Work/index.tsx
+++ components/Landing/Work/index.tsx
@@ -12,7 +12,7 @@ export default function Work() {
     <section id="work">
       <Element
         name="work"
-        className="grid w-screen grid-cols-2 gap-4 text-center"
+        className="grid w-screen grid-cols-2 gap-4 text-center hover:cursor-none"
       >
         <WorkTitle />
         {workLinks.slice(0, MAX_DISPLAY_WORKS).map((link) => (
