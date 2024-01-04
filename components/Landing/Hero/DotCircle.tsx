4f0a0ea81b6e82cb639a05c8554bfa1bd50caa17
Frad LEE <fradser@gmail.com>
2024-01-04 14:40:23 +0000

    fix(cmpts): dot circle animation

--- components/Landing/Hero/DotCircle.tsx
+++ components/Landing/Hero/DotCircle.tsx
@@ -1,4 +1,4 @@
-import { motion, useAnimationControls } from 'framer-motion'
+import { motion } from 'framer-motion'
 
 import { CursorProvider, CursorType } from '@/components/common/CursorProvider'
 
@@ -7,61 +7,30 @@ import {
   secondaryTransition,
 } from '@/utils/motion/springTransitions'
 
-interface IArrowProps {
-  controls: ReturnType<typeof useAnimationControls>
-  isInteractive?: boolean
-}
-
-function Arrow({ controls, isInteractive }: IArrowProps) {
-  if (!isInteractive) return null
-
-  return (
-    <motion.div
-      animate={controls}
-      className="absolute top-4 left-9 z-10 m-auto h-6 w-6 fill-white dark:fill-black"
-    >
-      <svg viewBox="0 0 18 46">
-        <path d="M0.439999 31.768C1.54933 32.1947 2.57333 32.664 3.512 33.176C4.49333 33.688 5.41067 34.264 6.264 34.904L6.264 0.599999L11.512 0.599999L11.512 34.904C12.3653 34.3067 13.2613 33.752 14.2 33.24C15.1813 32.728 16.2267 32.2587 17.336 31.832L17.336 36.696C14.2213 39.3413 11.8533 42.2427 10.232 45.4L7.48 45.4C5.944 42.2427 3.59733 39.3413 0.439999 36.696L0.439999 31.768Z" />
-      </svg>
-    </motion.div>
-  )
-}
-
 interface IDotCircleProps {
   isInteractive?: boolean
 }
 
 function DotCircle({ isInteractive }: IDotCircleProps) {
-  const controls = useAnimationControls()
-
-  const arrowVariants = {
-    initial: {
-      opacity: [0, 1],
-      transition: { ...primaryTransition, delay: 2.5 },
-    },
-    hoverInit: {
-      y: 0,
-      transition: { ...primaryTransition },
-    },
-    hoverAnim: {
-      y: [6, -6, 6],
-      transition: { ...secondaryTransition, repeat: Infinity },
-    },
-  }
-
   return (
     <CursorProvider targetCursorType={CursorType.headerLinkHovered}>
-      <motion.div
-        onViewportEnter={() => controls.start(arrowVariants.initial)}
-        onHoverStart={() => {
-          controls.start(arrowVariants.hoverAnim)
-        }}
-        onHoverEnd={() => {
-          controls.start(arrowVariants.hoverInit)
-        }}
-        className="absolute -bottom-20 -right-20 z-30 h-24 w-24 scale-75 hover:cursor-pointer lg:-bottom-20 lg:-right-24 lg:scale-100"
-      >
-        <Arrow controls={controls} isInteractive={isInteractive} />
+      <div className="absolute -bottom-20 -right-20 z-30 h-24 w-24 scale-75 hover:cursor-pointer lg:-bottom-20 lg:-right-24 lg:scale-100">
+        <motion.div
+          whileInView={{
+            opacity: [0, 1],
+            transition: { ...primaryTransition, delay: 2.5 },
+          }}
+          viewport={{ once: true }}
+          whileHover={{
+            scale: 1.1,
+            transition: { ...primaryTransition },
+          }}
+          className="absolute top-4 left-9 z-10 m-auto h-6 w-6 fill-white dark:fill-black"
+        >
+          <svg viewBox="0 0 18 46">
+            <path d="M0.439999 31.768C1.54933 32.1947 2.57333 32.664 3.512 33.176C4.49333 33.688 5.41067 34.264 6.264 34.904L6.264 0.599999L11.512 0.599999L11.512 34.904C12.3653 34.3067 13.2613 33.752 14.2 33.24C15.1813 32.728 16.2267 32.2587 17.336 31.832L17.336 36.696C14.2213 39.3413 11.8533 42.2427 10.232 45.4L7.48 45.4C5.944 42.2427 3.59733 39.3413 0.439999 36.696L0.439999 31.768Z" />
+          </svg>
+        </motion.div>
         <motion.div // Circle
           animate={{ height: ['20%', '100%'], width: ['20%', '100%'] }}
           transition={{ ...primaryTransition, delay: 2 }}
@@ -71,7 +40,7 @@ function DotCircle({ isInteractive }: IDotCircleProps) {
             <circle cx="48" cy="48" r="48" />
           </svg>
         </motion.div>
-      </motion.div>
+      </div>
     </CursorProvider>
   )
 }
