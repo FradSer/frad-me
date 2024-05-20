cc672f2a13421bb66dea2e71e4d94a1d360e413c
Frad LEE <fradser@gmail.com>
2024-05-20 19:38:53 +0800

    refactor(cmpts): make props readonly
    - Updated `WorkInformation` component in `WorkPage` to use `Readonly` for props.
    - Applied `Readonly` to `ILayoutWrapperProps` in `LayoutWrapper` component for immutable props.
    
    These changes enhance type safety by ensuring that component props are immutable.

--- components/WorkPage/WorkInformation.tsx
+++ components/WorkPage/WorkInformation.tsx
@@ -1,7 +1,9 @@
-export default function WorkInformation(props: {
-  title: string
-  data?: [string]
-}) {
+export default function WorkInformation(
+  props: Readonly<{
+    title: string
+    data?: [string]
+  }>,
+) {
   if (!props.data) return null
   return (
     <div className="col-span-8 md:col-span-3">
