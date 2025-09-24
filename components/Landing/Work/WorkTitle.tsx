d52d18ea19169189d191b346c48f43a4208ce83c
Frad LEE <fradser@gmail.com>
2025-09-25 00:31:48 +0800

    style: increase top spacing above work text

--- components/Landing/Work/WorkTitle.tsx
+++ components/Landing/Work/WorkTitle.tsx
@@ -13,7 +13,7 @@ export default function WorkTitle() {
         scale: scale,
         scrollMarginLeft: '0',
       }}
-      className="col-span-2 px-0 pt-8 text-center text-[7rem] hover:cursor-default sm:px-40 lg:text-[10rem] xl:text-[13rem] 2xl:text-[16rem]"
+      className="col-span-2 px-0 pt-24 text-center text-[7rem] hover:cursor-default sm:px-40 lg:text-[10rem] xl:text-[13rem] 2xl:text-[16rem]"
     >
       work
     </motion.h2>
